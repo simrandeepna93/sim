b2f9a27f362b2c9d0ef24b8a2b8f0287039b8709
simrandeepna93 <85513470+simrandeepna93@users.noreply.github.com>
2021-06-09 15:21:43 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- *Modifier:SIMRANDEEP SINGH
+ *Modifier:Simrandeep singh
  * STUDENT ID:991621182
  */
 public class CardTrick {
